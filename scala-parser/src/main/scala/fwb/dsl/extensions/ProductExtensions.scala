1508e56e903d13ac8ac92688b74618e334eba9d6
Pietras <p@pyetras.com>
2015-04-21 19:04:58 +0200

    without magnet

--- scala-parser/src/main/scala/fwb/dsl/extensions/ProductExtensions.scala
+++ scala-parser/src/main/scala/fwb/dsl/extensions/ProductExtensions.scala
@@ -20,28 +20,19 @@ import scala.language.reflectiveCalls
  */
 
 trait ProductExtensions extends Symbols {
-  trait AtMagnet[B1 <: HList, LU] {
-    def apply(tree: Expression, or: SuperPosMapper[Prod[B1, LU], Prod[B1, LU], LU], typ: ArgType[LU]): Rep[LU]
-  }
-
-  implicit def natAsMagnet[B1 <: HList](i: Nat)(implicit at: At[B1, i.N], ti: ToInt[i.N]) : AtMagnet[B1, at.Out] = new AtMagnet[B1, at.Out] {
-    override def apply(tree: AST.Expression, or: SuperPosMapper[AST.Prod[B1, at.Out], AST.Prod[B1, at.Out], at.Out], typ: ArgType[at.Out]): Rep[at.Out] = tree match {
-      case Expr(ProductConstructor(seq)) => new Rep[at.Out] {
-        override val tree: AST.Expression = seq(ti.apply())
-      }
-    }
-  }
-
-  implicit def repiAsMagnet[B1 <: HList, LU](i: Rep[Int]): AtMagnet[B1, LU] = new AtMagnet[B1, LU] {
-    override def apply(tree: AST.Expression, or: SuperPosMapper[Prod[B1, LU], Prod[B1, LU], LU], typ: ArgType[LU]): Rep[LU] = new Rep[LU] {
-      override val tree: Expression = Apply(Stdlib.at, List(tree, i.tree))(typ)
-    }
-  }
 
   class ProductOperations[B1 <: HList, LU](val arg1: Rep[Prod[B1, LU]]) extends Operations[Prod[B1, LU]] {
     implicit def luType = arg1.getTpe.asInstanceOf[ProdType[B1, LU]].luType
 
-    def apply[T](t: T)(implicit magnet: T => AtMagnet[B1, LU]) = t(tree, op.to[LU], luType)
+    def apply(i: Nat)(implicit ti: ToInt[i.N], at: At[B1, i.N]) : Rep[at.Out] = tree match {
+      case Expr(ProductConstructor(seq)) => new Rep[at.Out] {
+        override val tree: AST.Expression = seq(ti())
+      }
+    }
+
+    def at(i: Rep[Int]): Rep[LU] = new Rep[LU] { //FIXME jakos?
+      override val tree: Expression = Apply(Stdlib.at, List(arg1.tree, i.tree))(luType)
+    }
   }
 
   implicit def productToProductOps[H <: HList, LU](p: Rep[Prod[H, LU]])(implicit typ: ProdType[H, LU]) = new ProductOperations[H, LU](p)
