58457475159cd98e1603e9cb2c4559a59bbfb665
Pietras <p@pyetras.com>
2015-06-23 21:38:19 +0200

    better travis?

--- lib/publish_drill.sh
+++ lib/publish_drill.sh
@@ -1,7 +1,7 @@
-#!/bin/bash 
+#!/bin/bash
 
-mkdir -p $HOME/.downloads
-cd $HOME/.downloads
+mkdir -p $HOME/downloads
+cd $HOME/downloads
 wget -N https://s3.amazonaws.com/relaks/drill-1.1.0-SNAPHOT.tgz
 mkdir -p $HOME/.m2/repository/org/apache/drill
 cd $HOME/.m2/repository/org/apache/drill && tar xzf $HOME/.downloads/drill-1.1.0-SNAPHOT.tgz
---
+++ relaks/lang/src/main/scala/relaks/lang/dsl/extensions/NativeFunExtensions.scala
@@ -0,0 +1,8 @@
+package relaks.lang.dsl.extensions
+
+/**
+ * Created by Pietras on 23/06/15.
+ */
+trait NativeFunExtensions {
+
+}
